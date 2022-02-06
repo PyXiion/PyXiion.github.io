4236e0b0d4ca60ca2cabbb5990a507b845121aa5
PyXiion <59997570+PyXiion@users.noreply.github.com>
2022-02-06 11:43:54 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
 var e = document.createElement('div');
-e.innerHTML = `
-<iframe width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1" title="Never Gonna Give You Up" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-`;
+e.innerHTML = 
+`
+<iframe id=lol width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1" title="Never Gonna Give You Up" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
+<style>
+#lol {
+	position: fixed;
+	top: 0px;
+	left: 0px;
+}
+</style>
+`;
 while(e.firstChild) {
     document.body.appendChild(e.firstChild);
 }
